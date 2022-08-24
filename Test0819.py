19420e38d6b470b1f8c91bb84739b4995a7913f8
DESKTOP-E9E0I2K\ehdgn <ehdgns333@naver.com>
2022-08-24 17:22:31 +0900

    -테스트용

---
+++ Test0819.py
@@ -0,0 +1,74 @@
+##1
+
+# a = 0.2467*12+4.159
+# print(int(a))
+
+##2
+
+# a,b,c = map(int,input("정수 3개를 입력 : ").split())
+# print("합계 = {0}".format(a+b+c))
+
+##3
+
+# korean, english, math, science = map(int,input("점수를 순서대로 입력하세요[국,영,수,과] : ").split())
+# result = int((korean+english+math)/4)
+# print("네 과목의 평균 점수는 : {0}점 입니다.".format(result))
+
+##4
+
+# s = "hello"
+# t = "python"
+
+# print(s+"! "+t)
+
+##5
+
+# num_str = "720"
+# print(int(num_str))
+
+##6
+# m_price = 48584
+# t_price = 48584*36
+
+# print("{0:,}원".format(t_price))
+
+##7
+# print("Marry\'s cosmetics")
+
+##8
+# print("신씨가 소리질렀다. \"도둑이야\".")
+
+##9
+# pin = "881020-1068234"
+# sp_pin = pin.split("-")
+#print("연월일 : "+sp_pin[0])
+# print("뒷자리 : "+sp_pin[1])
+
+##10
+# pin = "881020-1068234"
+# sp_pin = pin.split("-")
+# print(sp_pin[1][0])
+
+##11
+# s = "a@b@c@d"
+
+# new_s=s.replace("@","#",1)
+# new_s=new_s.replace("@","&",1)
+# new_s=new_s.replace("@","%",1)
+
+# print(new_s)
+
+##12
+# n1 = "Braund, Mr. Owen Harris"
+# n2 = "Moran, Mr. James"
+# n3 = "Bonnell, Miss. Elizabeth"
+
+# m = "Mr"
+# s = "Miss"
+
+# for i in [n1,n2,n3]:
+#     if i.find(m)>-1:
+#         print(i+"에서 Mr 추출")
+#     elif i.find(s)>-1:
+#         print(i+"에서 Miss 추출")
+
